271841c39167df6ac2d22a644c90b96fab3a49d7
haikeint <haikeint@gmail.com>
2024-09-13 00:20:35 +0700

    feat: add validate for phone number into secureRoute

--- src/components/Secure/FormPhoneNumber.vue
+++ src/components/Secure/FormPhoneNumber.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
-import { reactive, ref } from 'vue'
-import InputText from '../Util/InputText.vue'
-import LabelComp from '../Util/LabelComp.vue'
+import type { IInputEvent } from '@/interface/IInputEvent'
+import { reactive, ref, toRaw } from 'vue'
+// import InputText from '../Util/InputText.vue'
+import InputText from '@/components/InputText.vue'
+// import LabelComp from '../Util/LabelComp.vue'
 import type { IModal } from '../../interface/IModal'
 
 import { useMutation } from '@vue/apollo-composable'
@@ -11,7 +13,7 @@ const { mutate: changePhone } = useMutation(MUTATION_CHANGE_PHONE)
 const refModal = ref<IModal>()
 const loading = ref<boolean>(false)
 
-defineProps({
+const props = defineProps({
     isExisted: { type: Boolean, required: true }
 })
 
@@ -20,12 +22,43 @@ const phoneNumber = reactive({
     new: ''
 })
 
-const emit = defineEmits(['alert'])
+const validMessage: Record<string, string> = reactive({
+    oldPhone: '',
+    newPhone: ''
+})
 
-const update = () => {
-    refModal.value?.show()
+const validState = reactive({
+    oldPhoneClass: 'form-control',
+    newPhoneClass: 'form-control'
+})
+
+const validFn = {
+    checkOldPhone: (value: string) => {
+        let result = value.length == 0 ? 'SĐT không được để trống.' : ''
+        validMessage.oldPhone = result
+        validState.oldPhoneClass = result ? 'form-control invalid-txt' : 'form-control valid-txt'
+    },
+    checkNewPhone: (value: string) => {
+        let result = value.length == 0 ? 'SĐT không được để trống.' : ''
+        validMessage.newPhone = result
+        validState.newPhoneClass = result ? 'form-control invalid-txt' : 'form-control valid-txt'
+    }
 }
 
+const emit = defineEmits(['alert'])
+
+const checkInput = (event: IInputEvent) => {
+    const element = event.target as HTMLInputElement
+    return element.value.replace(/[^0-9]/g, '')
+}
+const update = () => {
+    // if (props.isExisted) validFn.checkOldPhone(phoneNumber.old)
+    validFn.checkNewPhone(phoneNumber.new)
+
+    if (!Object.values(validMessage).some((value) => Boolean(value))) {
+        refModal.value?.show()
+    }
+}
 const confirmedModal = () => {
     loading.value = true
     changePhone({
@@ -66,21 +99,35 @@ const confirmedModal = () => {
                 class="col-md-1 col-sm-1 d-none d-sm-flex justify-content-center align-items-center"
             ></div>
             <div class="col-md-6 col-sm-9">
-                <div class="mb-3" v-if="isExisted">
-                    <LabelComp to="phongnumber-current" text="SĐT hiện tại"></LabelComp>
+                <!-- <div class="mb-2" v-if="isExisted">
                     <InputText
-                        id="phongnumber-current"
-                        placeholder="0123123123"
-                        v-model:value="phoneNumber.old"
-                    ></InputText>
-                </div>
+                        id="ipres-old-phone"
+                        :className="validState.oldPhoneClass"
+                        label="SĐT hiện tại"
+                        placeholder="0900111222"
+                        :message="validMessage.oldPhone"
+                        :value="toRaw(phoneNumber.old)"
+                        :disabled="loading"
+                        :events="{
+                            blur: (event: IInputEvent) => validFn.checkOldPhone(event.target.value),
+                            input: (event: IInputEvent) => (phoneNumber.old = checkInput(event))
+                        }"
+                    />
+                </div> -->
                 <div class="mb-3">
-                    <LabelComp to="phongnumber-new" text="SĐT mới"></LabelComp>
                     <InputText
-                        id="phongnumber-new"
-                        placeholder="0123123123"
-                        v-model:value="phoneNumber.new"
-                    ></InputText>
+                        id="ipres-new-phone"
+                        :className="validState.newPhoneClass"
+                        label="SĐT mới"
+                        placeholder="0900111222"
+                        :message="validMessage.newPhone"
+                        :value="toRaw(phoneNumber.new)"
+                        :disabled="loading"
+                        :events="{
+                            blur: (event: IInputEvent) => validFn.checkOldPhone(event.target.value),
+                            input: (event: IInputEvent) => (phoneNumber.new = checkInput(event))
+                        }"
+                    />
                 </div>
                 <button class="btn btn-danger rounded-pill" @click="update">Cập nhật</button>
             </div>
